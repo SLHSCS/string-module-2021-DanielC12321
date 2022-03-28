bda6a214b534a5c4e8d03d180642bfd28e4f69bd
DanielC12321 <73292458+DanielC12321@users.noreply.github.com>
2022-03-28 07:57:26 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-hello world
+/**
+ * 
+ * 
+ * @param {
+ * String
+ * } st
+ * @returns 
+ */
+
+
+
+//1
+module.exports.removeSpaces = (st) => {
+
+return st.replaceAll(" ", "");
+
+
+}
+
+
+
+//2
+module.exports.containsAll() = (Arrayin, str) => {
+
+var Arrayout = [];
+
+for(const element of Arrayin)
+{
+
+
+    if(str.containsAll(element.toString()))
+    {
+        Arrayout.push(element);
+    }
+}
+return Arrayout;
+
+    
+    
+    }
